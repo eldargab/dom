0b9b11fd5dbe0f2502b1a2c20a2464b9f82d4ce8
Eldar Gabdullin <eldargab@gmail.com>
2013-09-25 14:52:48 +0400

    add jQuery like API

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ var domify = require('domify')
   , classes = require('classes')
   , event = require('event')
 
-module.exports = dom
+var dom = module.exports = new Dom(document)
 
-function dom (html, ctx) {
-  return html[0] == '<'
-    ? domify(html)
-    : (ctx || document).querySelector(html)
+function Dom(ctx) {
+  this.ctx = ctx
 }
 
-dom.find = function(sel, ctx) {
-  var list = (ctx || document).querySelectorAll(sel)
-  return Array.prototype.slice.call(list)
+Dom.prototype.get = function(sel, el) {
+  return (el || this.ctx).querySelector(sel)
+}
+
+Dom.prototype.fromHTML = function(html) {
+  return domify(html)
+}
+
+Dom.prototype.find = function(sel, el) {
+  return new List((el || this.ctx).querySelectorAll(sel))
+}
+
+Dom.prototype.scoped = function(el) {
+  return new Dom(el)
 }
 
 function next(el) {
@@ -29,35 +38,45 @@ function prev(el) {
   return el
 }
 
-dom.firstChild = function(el) {
+Dom.prototype.firstChild = function(el) {
   return next(el.firstChild)
 }
 
-dom.lastChild = function(el) {
+Dom.prototype.lastChild = function(el) {
   return prev(el.lastChild)
 }
 
-dom.next = function(el) {
+Dom.prototype.next = function(el) {
   return next(el.nextSibling)
 }
 
-dom.prev = function(el) {
+Dom.prototype.prev = function(el) {
   return prev(el.previousSibling)
 }
 
-dom.addClass = function (cl, el) {
+Dom.prototype.children = function(el) {
+  var arr = []
+    , ch = next(el.firstChild)
+  while(ch) {
+    arr.push(ch)
+    ch = next(ch.nextSibling)
+  }
+  return new List(arr)
+}
+
+Dom.prototype.addClass = function (cl, el) {
   classes(el).add(cl)
 }
 
-dom.removeClass = function (cl, el) {
+Dom.prototype.removeClass = function (cl, el) {
   classes(el).remove(cl)
 }
 
-dom.hasClass = function (cl, el) {
+Dom.prototype.hasClass = function (cl, el) {
   return classes(el).has(cl)
 }
 
-dom.toggle = function(cl, on, el) {
+Dom.prototype.toggle = function(cl, on, el) {
   if (on) {
     dom.addClass(cl, el)
   } else {
@@ -65,27 +84,80 @@ dom.toggle = function(cl, on, el) {
   }
 }
 
-dom.remove = function (el) {
+Dom.prototype.remove = function (el) {
   var parent = el.parentNode
   parent && parent.removeChild(el)
 }
 
-dom.replace = function (n, old) {
+Dom.prototype.replace = function (n, old) {
   old.parentNode.replaceChild(n, old)
 }
 
-dom.insertBefore = function (n, ref) {
+Dom.prototype.insertBefore = function (n, ref) {
   ref.parentNode.insertBefore(n, ref)
 }
 
-dom.insertAfter = function (n, ref) {
+Dom.prototype.insertAfter = function (n, ref) {
   ref.parentNode.insertBefore(n, ref.nextSibling)
 }
 
-dom.on = function(el, type, fn, capture) {
+Dom.prototype.on = function(el, type, fn, capture) {
   return event.bind(el, type, fn, capture || false)
 }
 
-dom.off = function(el, type, fn, capture) {
+Dom.prototype.off = function(el, type, fn, capture) {
   return event.unbind(el, type, fn, capture || false)
+}
+
+function List(col) {
+  this.col = col
+}
+
+List.prototype.forEach = function(fn) {
+  for(var i = 0; i < this.col.length; i++) {
+    fn(this.col[i])
+  }
+  return this
+}
+
+List.prototype.map = function(fn) {
+  var ret = new Array(this.col.length)
+  for(var i = 0; i < this.col.length; i++) {
+    ret[i] = fn(this.col[i])
+  }
+  return ret
+}
+
+List.prototype.toArray = function() {
+  return Array.prototype.slice.call(this.col)
+}
+
+List.prototype.on = function(type, fn, capture) {
+  return this.forEach(function(el) {
+    dom.on(el, type, fn, capture)
+  })
+}
+
+List.prototype.off = function(type, fn, capture) {
+  return this.forEach(function(el) {
+    dom.off(el, type, fn, capture)
+  })
+}
+
+List.prototype.addClass = function(cl) {
+  return this.forEach(function(el) {
+    dom.addClass(cl, el)
+  })
+}
+
+List.prototype.removeClass = function(cl) {
+  return this.forEach(function(el) {
+    dom.removeClass(cl, el)
+  })
+}
+
+List.prototype.toggle = function(cl, on) {
+  return this.forEach(function(el) {
+    dom.toggle(cl, on, el)
+  })
 }
--- test/dom.js
+++ test/dom.js
@@ -1,27 +1,41 @@
 var dom = require('dom')
 var assert = require('component-assert')
 
-describe('dom()', function () {
-  describe('with html', function () {
-    it('should convert html to element', function () {
-      var el = dom('<div>Hello</div>')
-      assert(el.textContent == 'Hello')
-    })
+describe('.fromHTML()', function () {
+  it('should convert html to element', function () {
+    var el = dom.fromHTML('<div>Hello</div>')
+    assert(el.textContent == 'Hello')
   })
+})
 
-  describe('with selector', function () {
-    it('should return matched element', function () {
-      var foo = dom('#foo')
-      assert(foo.nodeType == 1)
-      var bar = dom('.bar', foo)
-      assert(bar.textContent == 'Hello')
-    })
+describe('.get(sel, [ctx])', function () {
+  it('should return matched element', function () {
+    var foo = dom.get('#foo')
+    assert(foo.nodeType == 1)
+    var bar = dom.get('.bar', foo)
+    assert(bar.textContent == 'Hello')
+  })
+})
+
+describe('.scoped(el)', function() {
+  it('should return dom with query API scoped to the given el', function() {
+    var root = dom.fromHTML(
+      '<div>' +
+        '<p>1</p>' +
+        '<div class="second">' +
+          '<p>2</p>' +
+        '</div>' +
+      '</div>'
+    )
+    var p = dom.scoped(dom.get('.second', root)).get('p')
+    assert(p.textContent == '2')
+    assert(dom.get('p', root).textContent == '1')
   })
 })
 
 describe('.firstChild()', function () {
   it('should return first element child', function () {
-    var el = dom('#foo')
+    var el = dom.get('#foo')
     var child = dom.firstChild(el)
     assert(child.nodeType == 1)
   })
@@ -29,16 +43,32 @@ describe('.firstChild()', function () {
 
 describe('.lastChild()', function() {
   it('should return last element child', function () {
-    var el = dom('#foo')
+    var el = dom.get('#foo')
     var child = dom.lastChild(el)
     assert(child.nodeType == 1)
   })
 })
 
+describe('.children(el)', function() {
+  it('should return a list of child elements', function() {
+    var el = dom.fromHTML(
+      '<div id="foo">' +
+        '<p>Hello</p>' +
+        '<!--comment-->' +
+        '<p>World</p>' +
+      '</div>'
+    )
+    var arr = dom.children(el).toArray()
+    assert(arr[0].textContent == 'Hello')
+    assert(arr[1].textContent == 'World')
+    assert(arr.length == 2)
+  })
+})
+
 describe('.remove()', function () {
   it('should remove element', function () {
-    var html = dom('<div><p>Hello</p></div>')
-    var el = dom('p', html)
+    var html = dom.fromHTML('<div><p>Hello</p></div>')
+    var el = dom.get('p', html)
     dom.remove(el)
     assert(html.textContent == '')
   })
@@ -46,25 +76,25 @@ describe('.remove()', function () {
 
 describe('.insertBefore(new, ref)', function () {
   it('should insert new before ref', function () {
-    var html = dom('<div><span> world</span></div>')
-    var world = dom('span', html)
-    dom.insertBefore(dom('<span>Hello</span>'), world)
+    var html = dom.fromHTML('<div><span> world</span></div>')
+    var world = dom.get('span', html)
+    dom.insertBefore(dom.fromHTML('<span>Hello</span>'), world)
     assert(html.textContent == 'Hello world')
   })
 })
 
 describe('.insertAfter()', function () {
   it('should insert new element after ref', function () {
-    var html = dom('<div><span>Hello</span></div>')
-    var hello = dom('span', html)
-    dom.insertAfter(dom('<span> world</span>'), hello)
+    var html = dom.fromHTML('<div><span>Hello</span></div>')
+    var hello = dom.get('span', html)
+    dom.insertAfter(dom.fromHTML('<span> world</span>'), hello)
     assert(html.textContent == 'Hello world')
   })
 })
 
 describe('.addClass()', function () {
   it('should add class to element', function () {
-    var el = dom('<div>')
+    var el = dom.fromHTML('<div>')
     dom.addClass('foo', el)
     assert(el.className == 'foo')
   })
@@ -72,7 +102,7 @@ describe('.addClass()', function () {
 
 describe('.removeClass()', function () {
   it('should remove class from element', function () {
-    var el = dom('<div class="foo bar baz">')
+    var el = dom.fromHTML('<div class="foo bar baz">')
     dom.removeClass('foo', el)
     assert(el.className == 'bar baz')
   })
@@ -80,7 +110,7 @@ describe('.removeClass()', function () {
 
 describe('.hasClass()', function () {
   it('should check for presence of class', function () {
-    var el = dom('<div class="foo bar">')
+    var el = dom.fromHTML('<div class="foo bar">')
     assert(dom.hasClass('bar', el))
     assert(!dom.hasClass('baz', el))
   })
@@ -88,7 +118,7 @@ describe('.hasClass()', function () {
 
 describe('.toggle(cl, on, el)', function() {
   it('when "on" should add class', function() {
-    var el = dom('<div class="foo">')
+    var el = dom.fromHTML('<div class="foo">')
     dom.toggle('hello', true, el)
     dom.toggle('foo', true, el)
     assert(dom.hasClass('hello', el))
@@ -96,7 +126,7 @@ describe('.toggle(cl, on, el)', function() {
   })
 
   it('when "off" should remove class', function() {
-    var el = dom('<div class="foo">')
+    var el = dom.fromHTML('<div class="foo">')
     dom.toggle('foo', false, el)
     dom.toggle('bar', false, el)
     assert(!dom.hasClass('foo', el))
